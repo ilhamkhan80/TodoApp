49ee8ceb10f5bfa686ecc63dab134fe1872afd3c
ilhamkhan80 <khanilham301@gmail.com>
2024-08-10 12:14:16 +0500

    TodoApp

--- App.tsx
+++ App.tsx
@@ -1,10 +1,21 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import Weather from './src/screens/Weather'
-
+import {NavigationContainer } from '@react-navigation/native'
+import  {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Todo from './src/screens/Todo'
+import New from './src/screens/New'
+const Stack = createNativeStackNavigator();
 const App = () => {
   return (
-    <Weather/>
+    <NavigationContainer>
+    <Stack.Navigator
+    
+      screenOptions={{headerShown: false}}>
+      <Stack.Screen name="Todo" component={Todo} />
+      <Stack.Screen name="New" component={New} />
+    
+    </Stack.Navigator>
+  </NavigationContainer>
   )
 }
 
